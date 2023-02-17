58d02a4d09bd0986996bf3cc92d6892bea618e16
Ethosa <social.ethosa@gmail.com>
2023-02-17 13:12:41 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(
     name='hapdoc',
     description='autodoc tool for everything',
     long_description=long_description,
+    author='Ethosa',
+    author_email='social.ethosa@gmail.com',
+    maintainer='HapticX',
+    maintainer_email='hapticx.company@gmail.com',
+    url='https://github.com/HapticX/hapdoc',
     version='0.4',
     packages=find_packages(),
     py_modules=['hapdoc'],
@@ -15,5 +20,15 @@ setup(
         'console_scripts': [
             'hapdoc = hapdoc.hapdoc:hapdoc'
         ]
-    }
+    },
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: FastAPI',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Documentation'
+    ]
 )
