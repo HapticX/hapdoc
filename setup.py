dcc26402f3aa954dde949727676e83344c7fbf9e
Ethosa <social.ethosa@gmail.com>
2023-02-17 14:22:44 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.10',
-        'Topic :: Documentation'
+        'Topic :: Documentation',
         'Topic :: Software Development :: Documentation'
     ]
 )
