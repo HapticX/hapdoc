5a87f36236c0af86504fbace0d830bca6d4fde2c
Ethosa <social.ethosa@gmail.com>
2023-03-07 12:47:29 +0700

    little update

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 
-with open('./readme.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='hapdoc',
     description='autodoc tool for everything',
-    long_description=long_description,
     author='Ethosa',
     author_email='social.ethosa@gmail.com',
     maintainer='HapticX',
     maintainer_email='hapticx.company@gmail.com',
     url='https://github.com/HapticX/hapdoc',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     py_modules=['hapdoc'],
     install_requires=['click', 'colorama', 'fastapi', 'uvicorn'],
